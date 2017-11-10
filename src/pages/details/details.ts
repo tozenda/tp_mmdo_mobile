7e85b035727b021585f12552a024cc37feca986c
tozenda <thom.ozenda@gmail.com>
2017-11-10 16:36:55 +0100

    UNE LISTE ! GÉNIAL

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-details',
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgForOf } from '@angular/common';
+
+interface item{
+  title:string;
+  author:string;
+  date:string;
+  image:string;
+}
+
+const i: item = { title: 'Lorem Ipsum', author: 'moi lol', date: '01/01/1990', image: "https://placeimg.com/50/50/any"};
+
+const items: item[] = [i,i,i,i,i,i,i,i,i,i,i,i];
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  results : item[] = items;
 }
